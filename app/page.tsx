bf3787551c8acf4468273b4b40cbca4efd64c8d8
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-25 18:25:52 +0000

    PatientForm added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/*TODO: OTP Verification -> pass code */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -19,11 +20,11 @@ export default function Home() {
           
           />
           <div className="text-14-regular mt-20 flex justify-between ">
-          <p className="justify-items-end text-dark-600 xl:text-left">© 2025 Carepulse</p>
-          <Link href={"/?admin=true"} className="text-green-500">
-            Admin
-          </Link>
-          </div>
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2025 Carepulse</p>
+            <Link href={"/?admin=true"} className="text-green-500">
+              Admin
+            </Link>
+            </div>
         </div>
       </section>
 
