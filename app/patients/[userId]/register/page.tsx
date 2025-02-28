87e2241bb57d2eb25bd6e3b3fe71a70fe97a291f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-28 14:42:35 +0000

    installing radiogroup and select, updating the jsx

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,8 +1,13 @@
+import RegisterForm from '@/components/forms/RegisterForm'
+import { getUser } from '@/lib/actions/patient.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Register = () => {
+const Register = async ({params: {userId} } : SearchParamProps) => {//receive from url params
+
+  const user = await getUser(userId) 
+
   return (
     <div className="flex h-screen max-h-screen">
     {/*TODO: OTP Verification -> pass code */}
@@ -16,7 +21,7 @@ const Register = () => {
           className="mb-12 h-10 w-fit"
         />
         
-        {/* <PatientForm /> */}
+        <RegisterForm user={user} />
 
         <div className="text-14-regular mt-20 flex justify-between ">
           <p className="justify-items-end text-dark-600 xl:text-left">© 2025 Carepulse</p>
