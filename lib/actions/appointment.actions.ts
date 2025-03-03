4a3591d3b571abc6e44fae3838cc32adaf6f8cf4
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-03 12:09:19 +0000

    success page completed

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -19,3 +19,17 @@ export const createAppointment =  async(appointment: CreateAppointmentParams) =>
 
    }
 }
+
+export const getAppointment = async(appointmentId: string) => {
+   try {
+      const appointment = await databases.getDocument(
+         DATABASE_ID!,
+         APPOINTMENT_COLLECTION_ID!,
+         appointmentId
+      )
+
+      return parseStringify(appointment)
+   } catch(error){
+
+   }
+}
