a551c9b45718fb82e403d81e1ce9fb9cf296970b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-04 20:59:21 +0000

    getRecentAppointmentList

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link'
 import React from 'react'
 
 const Admin = () => {
+   
+
+
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
       <header className='admin-header'>
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 "use server"
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+import { parse } from "path";
 
 export const createAppointment =  async(appointment: CreateAppointmentParams) => {
    try {
@@ -31,5 +33,45 @@ export const getAppointment = async(appointmentId: string) => {
       return parseStringify(appointment)
    } catch(error){
 
+   }
+}
+
+export const getRecentAppointmentList = async() => {
+   try {
+      const appointments = await databases.listDocuments(
+         DATABASE_ID!,
+         APPOINTMENT_COLLECTION_ID!,
+         [Query.orderDesc("$createdAt")], // order by createdAt in descending order
+      )
+
+      const initialCounts = {
+         scheduledCount: 0,
+         pendingCount:0,
+         cancelledCount: 0,
+      }
+
+      const counts = ( appointments.documents as Appointment[]).reduce((acc, appointment) => {
+         if(appointment.status === "scheduled") {
+            acc.scheduledCount += 1;
+         } else if (appointment.status === "pending") {
+            acc.pendingCount += 1;
+         } else if (appointment.status === "cancelled") {
+            acc.cancelledCount +=1;
+         }
+
+         return acc; // return the updated counts object
+      },initialCounts)
+
+      const data = {
+         totalCount: appointments.total,
+         ...counts, // scheduledCount, pendingCount, cancelledCount
+         documents: appointments.documents
+      }
+
+      return parseStringify(data)
+
+
+   } catch(error) {
+      console.log(error)
    }
 }
